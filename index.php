c0f6f84e6ba8094ed0d37f4458f4b3b3c7112c9a
DENIS <DENIS@192.168.36.36>
2017-02-02 16:02:00 -0500

    Deniseditado

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     	<body>
         	<?php
         		require_once('controllers/index.php');
-                        echo'esto edito Denis';        	?>
+                        echo'esto edito Denis Espinoza';        	?>
     	</body>
 </html>
